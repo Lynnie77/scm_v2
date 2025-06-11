c64de0fc77cfdc7d003bfca6d437e9d685c60c13
unknown <geoffsglass@hotmail.com>
2025-06-11 13:16:16 -0800

    Added a grid to main_wrapper, nav and hamburger btn fit nice, carousel when turns does a funny adjusting to scroll bar.  might be a bug might be considred a feature. Everythings lined up pretty good

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,4 @@
 document.getElementById("hamburger").addEventListener("click", () => {
   document.getElementById("navLinks").classList.toggle("nav-active");
+  document.getElementById("hamburger").classList.toggle("active");
 });
